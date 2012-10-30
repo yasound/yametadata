687af452820d8b565c97f5b05c2fe42ee7ca99ad
Jérôme Blondon <jerome@yasound.com>
2012-10-30 09:26:32 +0100

    kfm: added cover & large cover

--- yametadata/kfm.py
+++ yametadata/kfm.py
@@ -2,9 +2,22 @@ import xmltodict
 import requests
 
 KFM_BASE_COVER = 'http://www.k-fm.com/wp-content/plugins/4ways/_radio/pochettes/'
-KFM_COVER_SIZE = 210
+KFM_COVER_SIZE_NORMAL = 32
+KFM_COVER_SIZE_LARGE = 210
 
-def find_metadata(cover_size=KFM_COVER_SIZE):
+
+def find_metadata():
+    """Return the current song.
+    the returned data is a dict::
+
+        dict = {
+            'name': 'foo', # title
+            'artist': 'bla' # artist name
+            'cover': 'http://cover_normal', # url of normal cover
+            'large_cover': 'http://cover_large' # url of large cover
+        }
+
+    """
     url = 'http://static.k-fm.com/_radio/titrage.xml'
     params = {
     }
@@ -30,14 +43,18 @@ def find_metadata(cover_size=KFM_COVER_SIZE):
         current_radio = data['radio']['current']
         artist = current_radio.get('artist')
         name = current_radio.get('title')
-        cover = current_radio.get('picture')
-        if cover:
-            cover = u'%s%s~%s' % (KFM_BASE_COVER, cover, cover_size)
+        cover_file = current_radio.get('picture')
+        cover = None
+        large_cover = None
+        if cover_file:
+            cover = u'%s%s~%s' % (KFM_BASE_COVER, cover_file, KFM_COVER_SIZE_NORMAL)
+            large_cover = u'%s%s~%s' % (KFM_BASE_COVER, cover_file, KFM_COVER_SIZE_NORMAL)
 
         data = {
             'artist': artist,
             'name': name,
-            'cover': cover
+            'cover': cover,
+            'large_cover': large_cover
         }
 
     except:
